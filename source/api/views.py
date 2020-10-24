f9c8d798fe28a340b886fad85516e60f17c5b70e
RagdIR <ragdirstrogov@gmail.com>
2020-10-24 19:29:29 +0600

    Обновлены фикстуры и вьюшка

--- source/api/views.py
+++ source/api/views.py
@@ -1,34 +1,13 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
-from django.views.generic import View
+
 from pip._vendor.requests import Response
 from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 
 from webapp.models import Photo, Favorite
 
-
-# class AddFavView(LoginRequiredMixin, View):
-#     def post(self, request, *args, **kwargs):
-#         photo = get_object_or_404(Photo, pk=kwargs.get('pk'))
-#         favorite, created = Favorite.objects.get_or_create(photo=photo, user=request.user)
-#         if created:
-#             photo.favorite
-#             photo.save()
-#             return HttpResponse(photo.favorite)
-#         else:
-#             return HttpResponseForbidden()
-#
-#
-# class DelFavView(LoginRequiredMixin, View):
-#     def delete(self, request, *args, **kwargs):
-#         photo = get_object_or_404(Photo, pk=kwargs.get('pk'))
-#         favorite = get_object_or_404(photo.favorites, user=request.user)
-#         favorite.delete()
-#         photo.save()
-#         return HttpResponse(photo.favorite)
 
 @ensure_csrf_cookie
 def get_token_view(request, *args, **kwargs):
@@ -44,7 +23,7 @@ class PhotoViewSet(ViewSet):
         photo = get_object_or_404(Photo, pk=pk)
         add, created = Favorite.objects.get_or_create(photo=photo, user=request.user)
         if created:
-            photo.add_favorite
+            photo.favorite
             photo.save()
             return Response({'pk': pk, 'favorites': photo.add_favorite})
         else:
@@ -53,8 +32,8 @@ class PhotoViewSet(ViewSet):
     @action(methods=['delete'], detail=True)
     def unfavor(self, request, pk=None):
         photo = get_object_or_404(Photo, pk=pk)
-        add = get_object_or_404(photo.favorite, user=request.user)
+        add = get_object_or_404(photo.favorites, user=request.user)
         add.delete()
-        photo.add_favorite
+        photo.favorite
         photo.save()
         return Response({'pk': pk, 'favorites': photo.add_favorite})
